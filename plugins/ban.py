3ad03b68b78d5a00e3dfb1a930bfdea1a565f687
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2023-12-24 08:41:53 +0530

    Create ban.py

---
+++ plugins/ban.py
@@ -0,0 +1,218 @@
+from pyrogram import (
+    Client,
+    filters
+)
+from pyrogram.types import (
+    ChatPermissions
+)
+from pyrogram import Client, filters
+from functions.help_func.admin_check import admin_check
+from functions.help_func.extract_user import extract_user
+from functions.help_func.string_handling import extract_time
+
+
+@Client.on_message(filters.command("ban"))
+async def ban_user(_, message):
+    is_admin = await admin_check(message)
+    if not is_admin:
+        return
+
+    user_id, user_first_name = extract_user(message)
+
+    try:
+        await message.chat.kick_member(
+            user_id=user_id
+        )
+    except Exception as error:
+        await message.reply_text(
+            str(error)
+        )
+    else:
+        if str(user_id).lower().startswith("@"):
+            await message.reply_text(
+                "Someone else is dusting off..! "
+                f"{user_first_name}"
+                " Is forbidden."
+            )
+        else:
+            await message.reply_text(
+                "Someone else is dusting off..! "
+                f"<a href='tg://user?id={user_id}'>"
+                f"{user_first_name}"
+                "</a>"
+                " Is forbidden."
+            )
+
+
+@Client.on_message(filters.command("tban"))
+async def temp_ban_user(_, message):
+    is_admin = await admin_check(message)
+    if not is_admin:
+        return
+
+    if not len(message.command) > 1:
+        return
+
+    user_id, user_first_name = extract_user(message)
+
+    until_date_val = extract_time(message.command[1])
+    if until_date_val is None:
+        await message.reply_text(
+            (
+                "Invalid time type specified. "
+                "Expected m, h, or d, Got it: {}"
+            ).format(
+                message.command[1][-1]
+            )
+        )
+        return
+
+    try:
+        await message.chat.kick_member(
+            user_id=user_id,
+            until_date=until_date_val
+        )
+    except Exception as error:
+        await message.reply_text(
+            str(error)
+        )
+    else:
+        if str(user_id).lower().startswith("@"):
+            await message.reply_text(
+                "Someone else is dusting off..! "
+                f"{user_first_name}"
+                f" banned for {message.command[1]}!"
+            )
+        else:
+            await message.reply_text(
+                "Someone else is dusting off..! "
+                f"<a href='tg://user?id={user_id}'>"
+                "Lavane"
+                "</a>"
+                f" banned for {message.command[1]}!"
+            )
+
+
+
+
+
+@Client.on_message(filters.command(["unban", "unmute"]))
+async def un_ban_user(_, message):
+    is_admin = await admin_check(message)
+    if not is_admin:
+        return
+
+    user_id, user_first_name = extract_user(message)
+
+    try:
+        await message.chat.unban_member(
+            user_id=user_id
+        )
+    except Exception as error:
+        await message.reply_text(
+            str(error)
+        )
+    else:
+        if str(user_id).lower().startswith("@"):
+            await message.reply_text(
+                "Okay, changed ... now "
+                f"{user_first_name} To "
+                " You can join the group!"
+            )
+        else:
+            await message.reply_text(
+                "Okay, changed ... now "
+                f"<a href='tg://user?id={user_id}'>"
+                f"{user_first_name}"
+                "</a> To "
+                " You can join the group!"
+            )
+
+
+
+
+@Client.on_message(filters.command("mute"))
+async def mute_user(_, message):
+    is_admin = await admin_check(message)
+    if not is_admin:
+        return
+
+    user_id, user_first_name = extract_user(message)
+
+    try:
+        await message.chat.restrict_member(
+            user_id=user_id,
+            permissions=ChatPermissions(
+            )
+        )
+    except Exception as error:
+        await message.reply_text(
+            str(error)
+        )
+    else:
+        if str(user_id).lower().startswith("@"):
+            await message.reply_text(
+                "👍🏻 "
+                f"{user_first_name}"
+                " Lavender's mouth is shut! 🤐"
+            )
+        else:
+            await message.reply_text(
+                "👍🏻 "
+                f"<a href='tg://user?id={user_id}'>"
+                "Of lavender"
+                "</a>"
+                " The mouth is closed! 🤐"
+            )
+
+
+@Client.on_message(filters.command("tmute"))
+async def temp_mute_user(_, message):
+    is_admin = await admin_check(message)
+    if not is_admin:
+        return
+
+    if not len(message.command) > 1:
+        return
+
+    user_id, user_first_name = extract_user(message)
+
+    until_date_val = extract_time(message.command[1])
+    if until_date_val is None:
+        await message.reply_text(
+            (
+                "Invalid time type specified. "
+                "Expected m, h, or d, Got it: {}"
+            ).format(
+                message.command[1][-1]
+            )
+        )
+        return
+
+    try:
+        await message.chat.restrict_member(
+            user_id=user_id,
+            permissions=ChatPermissions(
+            ),
+            until_date=until_date_val
+        )
+    except Exception as error:
+        await message.reply_text(
+            str(error)
+        )
+    else:
+        if str(user_id).lower().startswith("@"):
+            await message.reply_text(
+                "Be quiet for a while! 😠"
+                f"{user_first_name}"
+                f" muted for {message.command[1]}!"
+            )
+        else:
+            await message.reply_text(
+                "Be quiet for a while! 😠"
+                f"<a href='tg://user?id={user_id}'>"
+                "Of lavender"
+                "</a>"
+                " Mouth "
+                f" muted for {message.command[1]}!"
+            )
